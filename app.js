37cb8624ac6ea6cdb706808a906d9a2c00aa6fe0
zhr85210078 <zhr85210078@163.com>
2017-09-26 10:52:49 +0800

    node-mongodb-es-connector

---
+++ app.js
@@ -0,0 +1,18 @@
+var MONGO_OPLOG_URL="mongodb://localhost:29031/local"; 
+var MONGO_DATA_URL="mongodb://localhost:29031/myTest"; 
+
+var ELASTIC_SEARCH_URL="localhost:9200"; 
+var BATCH_COUNT = 5000; 
+
+process.env.DEBUG='*';
+process.env.MONGO_OPLOG_URL = MONGO_OPLOG_URL;
+process.env.MONGO_DATA_URL = MONGO_DATA_URL;
+process.env.ELASTIC_SEARCH_URL =ELASTIC_SEARCH_URL;
+process.env.BATCH_COUNT = BATCH_COUNT;
+
+var path = require("path");
+var ESMongoSync = require('./lib/main');
+var filePath =path.join(__dirname,'watchers');
+var getFileList = require('./lib/util/getFileList').readFileList(filePath,[]);
+ESMongoSync.init(getFileList,null,null);
+require('./lib/util/fsWatcher').fsWatcher(filePath,ESMongoSync,require('./lib/util/getFileList'));
