793dfbb79a7ce84ad4640c23817a95d4a54b5de6
zhr85210078 <zhr85210078@163.com>
2018-01-09 16:39:54 +0800

    node-mongodb-es-connector

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ var main = require('./lib/main');
 
 var start = function (filePath) {
     var getFileList = require('./lib/util/util').readFileList(filePath, []);
-    main.init(getFileList,filePath);
+    main.init(getFileList, filePath);
 };
 
-var addSingleWatcher = function (filePath, collectionName,filterQueryFilds, searchReturnFilds, defaultValueFilds,oplogUrl,dataUrl,documentsinBatch,index,type,esUrl) {
+var addSingleWatcher = function (filePath, collectionName, filterQueryFilds, searchReturnFilds, defaultValueFilds, oplogUrl, dataUrl, documentsinBatch, index, type, esUrl) {
     var flag = false;
     try {
         var watcher = {};
-        watcher.mongodb.mongodb_collectionName=collectionName;
-        watcher.mongodb.mongodb_filterQueryFilds=filterQueryFilds;
-        watcher.mongodb.mongodb_searchReturnFilds=searchReturnFilds;
-        watcher.mongodb.mongodb_defaultValueFilds=defaultValueFilds;
-        watcher.mongodb.mongodb_oplog_url=oplogUrl;
-        watcher.mongodb.mongodb_data_url=dataUrl;
-        watcher.mongodb.mongodb_documentsinBatch=documentsinBatch;
-        watcher.elasticsearch.elasticsearch_index=index;
-        watcher.elasticsearch.elasticsearch_type=type;
-        watcher.elasticsearch.elasticsearch_url=esUrl;
+        watcher.mongodb = {};
+        watcher.mongodb.mongodb_collectionName = collectionName;
+        watcher.mongodb.mongodb_filterQueryFilds = filterQueryFilds;
+        watcher.mongodb.mongodb_searchReturnFilds = searchReturnFilds;
+        watcher.mongodb.mongodb_defaultValueFilds = defaultValueFilds;
+        watcher.mongodb.mongodb_oplog_url = oplogUrl;
+        watcher.mongodb.mongodb_data_url = dataUrl;
+        watcher.mongodb.mongodb_documentsinBatch = documentsinBatch;
+        watcher.elasticsearch = {};
+        watcher.elasticsearch.elasticsearch_index = index;
+        watcher.elasticsearch.elasticsearch_type = type;
+        watcher.elasticsearch.elasticsearch_url = esUrl;
 
-        var file = path.join(filePath, collectionName+"_"+index + '.json');
+        var file = path.join(filePath, collectionName + "_" + index + '.json');
         fs.writeFileSync(file, JSON.stringify(watcher, null, '\t'));
         flag = true;
     }
@@ -33,22 +35,24 @@ var addSingleWatcher = function (filePath, collectionName,filterQueryFilds, sear
     return flag;
 };
 
-var updateSingleWatcher = function (filePath, collectionName,filterQueryFilds, searchReturnFilds, defaultValueFilds,oplogUrl,dataUrl,documentsinBatch,index,type,esUrl) {
+var updateSingleWatcher = function (filePath, collectionName, filterQueryFilds, searchReturnFilds, defaultValueFilds, oplogUrl, dataUrl, documentsinBatch, index, type, esUrl) {
     var flag = false;
     try {
         var watcher = {};
-        watcher.mongodb.mongodb_collectionName=collectionName;
-        watcher.mongodb.mongodb_filterQueryFilds=filterQueryFilds;
-        watcher.mongodb.mongodb_searchReturnFilds=searchReturnFilds;
-        watcher.mongodb.mongodb_defaultValueFilds=defaultValueFilds;
-        watcher.mongodb.mongodb_oplog_url=oplogUrl;
-        watcher.mongodb.mongodb_data_url=dataUrl;
-        watcher.mongodb.mongodb_documentsinBatch=documentsinBatch;
-        watcher.elasticsearch.elasticsearch_index=index;
-        watcher.elasticsearch.elasticsearch_type=type;
-        watcher.elasticsearch.elasticsearch_url=esUrl;
+        watcher.mongodb = {};
+        watcher.mongodb.mongodb_collectionName = collectionName;
+        watcher.mongodb.mongodb_filterQueryFilds = filterQueryFilds;
+        watcher.mongodb.mongodb_searchReturnFilds = searchReturnFilds;
+        watcher.mongodb.mongodb_defaultValueFilds = defaultValueFilds;
+        watcher.mongodb.mongodb_oplog_url = oplogUrl;
+        watcher.mongodb.mongodb_data_url = dataUrl;
+        watcher.mongodb.mongodb_documentsinBatch = documentsinBatch;
+        watcher.elasticsearch = {};
+        watcher.elasticsearch.elasticsearch_index = index;
+        watcher.elasticsearch.elasticsearch_type = type;
+        watcher.elasticsearch.elasticsearch_url = esUrl;
 
-        var file = path.join(filePath, collectionName+"_"+index + '.json');
+        var file = path.join(filePath, collectionName + "_" + index + '.json');
         fs.writeFileSync(file, JSON.stringify(watcher, null, '\t'));
         flag = true;
     }
@@ -58,12 +62,12 @@ var updateSingleWatcher = function (filePath, collectionName,filterQueryFilds, s
     return flag;
 };
 
-var deleteSingleWatcher = function (filePath, collectionName,index) {
+var deleteSingleWatcher = function (filePath, collectionName, index) {
     var flag = false;
     try {
         var files = fs.readdirSync(filePath);
         files.forEach(function (filename) {
-            if (filename == collectionName+"_"+index + '.json') {
+            if (filename == collectionName + "_" + index + '.json') {
                 fs.unlinkSync(path.join(filePath, filename));
                 flag = true;
             }
@@ -75,12 +79,12 @@ var deleteSingleWatcher = function (filePath, collectionName,index) {
     return flag;
 };
 
-var isExistWatcher = function (filePath, collectionName,index) {
+var isExistWatcher = function (filePath, collectionName, index) {
     var flag = false;
     try {
         var files = fs.readdirSync(filePath);
         files.forEach(function (filename) {
-            if (filename == collectionName+"_"+index + '.json') {
+            if (filename == collectionName + "_" + index + '.json') {
                 flag = true;
             }
         });
