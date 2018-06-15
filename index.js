a5500a0a9696ba93c6101832ef08e2a2858fe24c
zhr85210078 <zhr85210078@163.com>
2018-06-15 16:38:16 +0800

    node-mongodb-es-connector V2.0.13

--- index.js
+++ index.js
@@ -16,19 +16,25 @@ var addWatcher = function (fileName, obj) {
     var flag = false;
     try {
         var files = fs.readdirSync(currentFilePath);
-        files.forEach(function (name) {
-            var item = JSON.parse(fs.readFileSync(path.join(currentFilePath, name)));
-            if ((fileName + '.json') !== name) {
-                if ((item.elasticsearch.e_connection.e_server == obj.elasticsearch.e_connection.e_server &&
-                        item.elasticsearch.e_index != obj.elasticsearch.e_index) ||
-                    (item.elasticsearch.e_connection.e_server != obj.elasticsearch.e_connection.e_server &&
-                        item.elasticsearch.e_index == obj.elasticsearch.e_index)) {
-                    var file = path.join(currentFilePath, fileName + '.json');
-                    fs.writeFileSync(file, JSON.stringify(obj));
-                    flag = true;
+        if (files.length > 0) {
+            files.forEach(function (name) {
+                var item = JSON.parse(fs.readFileSync(path.join(currentFilePath, name)));
+                if ((fileName + '.json') !== name) {
+                    if ((item.elasticsearch.e_connection.e_server == obj.elasticsearch.e_connection.e_server &&
+                            item.elasticsearch.e_index != obj.elasticsearch.e_index) ||
+                        (item.elasticsearch.e_connection.e_server != obj.elasticsearch.e_connection.e_server &&
+                            item.elasticsearch.e_index == obj.elasticsearch.e_index)) {
+                        var file = path.join(currentFilePath, fileName + '.json');
+                        fs.writeFileSync(file, JSON.stringify(obj));
+                        flag = true;
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            var file = path.join(currentFilePath, fileName + '.json');
+            fs.writeFileSync(file, JSON.stringify(obj));
+            flag = true;
+        }
         return flag;
     } catch (error) {
         logger.errMethod(obj.elasticsearch.e_connection.e_server, obj.elasticsearch.e_index, "addWatcher error: " + error);
@@ -39,13 +45,19 @@ var updateWatcher = function (fileName, obj) {
     var flag = false;
     try {
         var files = fs.readdirSync(currentFilePath);
-        files.forEach(function (name) {
-            if ((fileName + '.json') === name) {
-                var file = path.join(currentFilePath, fileName + '.json');
-                fs.writeFileSync(file, JSON.stringify(obj));
-                flag = true;
-            }
-        });
+        if (files.length > 0) {
+            files.forEach(function (name) {
+                if ((fileName + '.json') === name) {
+                    var file = path.join(currentFilePath, fileName + '.json');
+                    fs.writeFileSync(file, JSON.stringify(obj));
+                    flag = true;
+                }
+            });
+        } else {
+            var file = path.join(currentFilePath, fileName + '.json');
+            fs.writeFileSync(file, JSON.stringify(obj));
+            flag = true;
+        }
         return flag;
     } catch (error) {
         logger.errMethod(obj.elasticsearch.e_connection.e_server, obj.elasticsearch.e_index, "updateWatcher error: " + error);
@@ -54,26 +66,28 @@ var updateWatcher = function (fileName, obj) {
 
 var deleteWatcher = function (fileName) {
     var flag = false;
+    var fileNameTotal = fileName + '.json';
     try {
         var files = fs.readdirSync(currentFilePath);
-        var newArray=[];
+        var newArray = [];
         files.forEach(function (name) {
-            if (fileName == name) {
-                fs.unlinkSync(path.join(currentFilePath, fileName));
+            if (fileNameTotal === name) {
+                var currentFileContent = require(path.join(currentFilePath, name));
+                fs.unlinkSync(path.join(currentFilePath, name));
                 flag = true;
-                var currentFileContent = require(path.join(filePath, name));
-                _.find(global.infoArray, function (file) {
-                    if (file.cluster === currentFileContent.elasticsearch.e_connection.e_server &&
-                        file.index === currentFileContent.elasticsearch.e_index) {
+                if (global.infoArray && global.infoArray.length > 0) {
+                    _.find(global.infoArray, function (file) {
+                        if (file.cluster === currentFileContent.elasticsearch.e_connection.e_server &&
+                            file.index === currentFileContent.elasticsearch.e_index) {
                             return;
-                    }
-                    else{
-                        newArray.push(file);
-                    }
-                });
+                        } else {
+                            newArray.push(file);
+                        }
+                    });
+                }
             }
         });
-        global.infoArray=newArray;
+        global.infoArray = newArray;
         return flag;
     } catch (error) {
         logger.errMethod("", obj.elasticsearch.e_index, "deleteWatcher error: " + error);
@@ -82,10 +96,11 @@ var deleteWatcher = function (fileName) {
 
 var isExistWatcher = function (fileName) {
     var flag = false;
+    var fileNameTotal = fileName + '.json';
     try {
         var files = fs.readdirSync(currentFilePath);
         files.forEach(function (name) {
-            if (fileName == name) {
+            if (fileNameTotal === name) {
                 flag = true;
             }
         });
@@ -96,7 +111,11 @@ var isExistWatcher = function (fileName) {
 };
 
 var getInfoArray = function () {
-    return global.infoArray;
+    if (global.infoArray) {
+        return global.infoArray;
+    } else {
+        return null;
+    }
 };
 
 module.exports = {
