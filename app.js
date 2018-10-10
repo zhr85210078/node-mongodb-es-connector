250493e4babffa8c0927203e69fd3436638baa11
zhr85210078 <zhr85210078@163.com>
2018-10-10 12:49:54 +0800

    node-mongodb-es-connector V2.1.18

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
  * @Author: horan 
  * @Date: 2017-07-09 10:24:01 
  * @Last Modified by: horan
- * @Last Modified time: 2018-09-21 14:31:03
+ * @Last Modified time: 2018-10-10 12:49:35
  * @Startup
  */
 
 var path = require("path");
-var filePath = path.join(__dirname, 'crawlerDataConfig');
+var filePath = path.join(__dirname, 'crawlerData');
 var getFileList = require('./lib/util/util').readFileList(filePath, [], ".json");
 require('./lib/util/util').createInfoArray(getFileList);
 require('./lib/main').init(getFileList, filePath);
