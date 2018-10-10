1bffe00ea87aca3d9eadaa6f40486dfc69a421c2
zhr85210078 <zhr85210078@163.com>
2018-10-10 12:48:45 +0800

    node-mongodb-es-connector V2.1.18

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
  * @Author: horan 
  * @Date: 2017-07-09 10:24:01 
  * @Last Modified by: horan
- * @Last Modified time: 2018-09-21 14:31:03
+ * @Last Modified time: 2018-10-10 11:35:26
  * @Startup
  */
 
-var path = require("path");
-var filePath = path.join(__dirname, 'crawlerDataConfig');
-var getFileList = require('./lib/util/util').readFileList(filePath, [], ".json");
-require('./lib/util/util').createInfoArray(getFileList);
-require('./lib/main').init(getFileList, filePath);
+// var path = require("path");
+// var filePath = path.join(__dirname, 'crawlerData');
+// var getFileList = require('./lib/util/util').readFileList(filePath, [], ".json");
+// require('./lib/util/util').createInfoArray(getFileList);
+// require('./lib/main').init(getFileList, filePath);
+
+
+
+var a = new Date("2018-07-20 13:44:00");
+
+var b = new Date("2018-07-20 13:44:00").getTime();
+
+var c = new Date(b);
+
+var d = new Date();
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
